7fa504ea9248b22e9eb69bd01dfdc532b84de1ff
BuildTools <unconfigured@null.spigotmc.org>
2022-03-01 03:24:18 +0700

    install bottom tabs

--- dev/App.js
+++ dev/App.js
@@ -1,15 +1,46 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Button} from 'react-native';
 import React, {useState} from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import {NavigationContainer as NavCon} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 const Stack = createNativeStackNavigator();
 
+function HomeScreen({navigation}) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+      <Button
+        title="To New"
+        onPress={() => {
+          navigation.navigate('New');
+        }}
+      />
+    </View>
+  );
+}
+
+function NewScreen({navigation}) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>New Screen</Text>
+      <Button
+        title="Back Home"
+        onPress={() => {
+          navigation.navigate('Home');
+        }}
+      />
+    </View>
+  );
+}
+
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <NavCon>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="New" component={NewScreen} />
+      </Stack.Navigator>
+    </NavCon>
   );
 };
 
