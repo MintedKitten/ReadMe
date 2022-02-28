1282ee8932b0f03a062b310734f61e532d96d9ef
BuildTools <unconfigured@null.spigotmc.org>
2022-03-01 06:01:03 +0700

    break; putting slide in tab nested stack

--- dev/App.js
+++ dev/App.js
@@ -3,10 +3,11 @@ import React, {useState} from 'react';
 import {NavigationContainer as NavCon} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
 function HomeScreen({navigation}) {
   return (
@@ -15,7 +16,7 @@ function HomeScreen({navigation}) {
       <Button
         title="To New"
         onPress={() => {
-          navigation.navigate('NewStack', {screen: 'New'});
+          navigation.navigate('New');
         }}
       />
     </View>
@@ -27,9 +28,23 @@ function NewScreen({navigation}) {
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>New Screen</Text>
       <Button
+        title="to More"
+        onPress={() => {
+          navigation.navigate('B', {screen: 'More'});
+        }}
+      />
+    </View>
+  );
+}
+
+function MoreScreen({navigation}) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>More Screen</Text>
+      <Button
         title="Back Home"
         onPress={() => {
-          navigation.navigate('HomeStack', {screen: 'Home'});
+          navigation.navigate('A', {screen: 'Home'});
         }}
       />
     </View>
@@ -42,7 +57,42 @@ function HomeStack() {
       <Stack.Screen
         name="Home"
         component={HomeScreen}
-        options={{headerShown: false, animation: 'fade'}}
+        options={{
+          animation: 'fade',
+          headerTitle: () => (
+            <Ionicons name="person-add" size={20} color="pink" />
+          ),
+          headerRight: () => (
+            <Text>
+              <Ionicons
+                name="person-add"
+                size={20}
+                color="brown"
+                onPress={() => {
+                  alert('1');
+                }}
+              />
+              {'  '}
+              <Ionicons
+                name="person-add"
+                size={20}
+                color="blue"
+                onPress={() => {
+                  alert('2');
+                }}
+              />
+              {'  '}
+              <Ionicons
+                name="person-add"
+                size={20}
+                color="green"
+                onPress={() => {
+                  alert('3');
+                }}
+              />
+            </Text>
+          ),
+        }}
       />
     </Stack.Navigator>
   );
@@ -56,66 +106,133 @@ function NewStack() {
   );
 }
 
+function HomeStackScreen() {
+  return (
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          animation: 'slide_from_left',
+        }}
+      />
+      <Stack.Screen
+        name="New"
+        component={NewScreen}
+        options={{
+          animation: 'slide_from_right',
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+function NewStackScreen() {
+  return (
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          animation: 'slide_from_left',
+        }}
+      />
+      <Stack.Screen
+        name="New"
+        component={NewScreen}
+        options={{
+          animation: 'slide_from_right',
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+function TestStackA() {
+  return (
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          animation: 'slide_from_left',
+        }}
+      />
+      <Stack.Screen
+        name="New"
+        component={NewScreen}
+        options={{
+          animation: 'slide_from_right',
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+function TestStackB() {
+  return (
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen
+        name="More"
+        component={MoreScreen}
+        options={{
+          animation: 'fade',
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+function MyTab() {
+  return (
+    <Tab.Navigator
+      initialRouteName="HomeStack"
+      shifting={true}
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName;
+          if (route.name === 'HomeStack') {
+            iconName = focused
+              ? 'ios-information-circle'
+              : 'ios-information-circle-outline';
+          } else if (route.name === 'NewStack') {
+            iconName = focused ? 'person-add-outline' : 'person-add';
+          }
+          return <Ionicons name={iconName} size={20} color={color} />;
+        },
+        // tabBarActiveTintColor: '#e91e63',
+        // tabBarInactiveTintColor: '#000',
+        tabBarColor: 'purple',
+      })}
+      backBehavior="history"
+      activeColor="blue"
+      inactiveColor="green">
+      <Tab.Screen
+        name="HomeStack"
+        component={HomeStack}
+        options={{
+          title: 'Woah',
+        }}
+      />
+      <Tab.Screen name="NewStack" component={NewStack} />
+    </Tab.Navigator>
+  );
+}
+
 const App = () => {
   return (
     <NavCon>
-      <Tab.Navigator
-        initialRouteName="HomeStack"
-        screenOptions={({route}) => ({
-          tabBarIcon: ({focused, color, size}) => {
-            let iconName;
-            if (route.name === 'HomeStack') {
-              iconName = focused
-                ? 'ios-information-circle'
-                : 'ios-information-circle-outline';
-            } else if (route.name === 'NewStack') {
-              iconName = focused ? 'person-add-outline' : 'person-add';
-            }
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-          tabBarActiveTintColor: '#e91e63',
-          tabBarInactiveTintColor: '#000',
-        })}>
-        <Tab.Screen
-          name="HomeStack"
-          component={HomeStack}
-          options={{
-            headerTitle: () => (
-              <Ionicons name="person-add" size={20} color="pink" />
-            ),
-            headerRight: () => (
-              <Text>
-                <Ionicons
-                  name="person-add"
-                  size={20}
-                  color="brown"
-                  onPress={() => {
-                    alert('1');
-                  }}
-                />{' '}
-                <Ionicons
-                  name="person-add"
-                  size={20}
-                  color="blue"
-                  onPress={() => {
-                    alert('2');
-                  }}
-                />{' '}
-                <Ionicons
-                  name="person-add"
-                  size={20}
-                  color="green"
-                  onPress={() => {
-                    alert('3');
-                  }}
-                />{' '}
-              </Text>
-            ),
-            title: 'Woah',
-          }}
-        />
-        <Tab.Screen name="NewStack" component={NewStack} />
+      {/* <MyTab /> */}
+
+      {/* <Tab.Navigator initialRouteName="HomeStack">
+        <Tab.Screen name="HomeStack" component={HomeStackScreen} />
+        <Tab.Screen name="NewStack" component={NewStackScreen} />
+      </Tab.Navigator> */}
+      <Tab.Navigator initialRouteName="A">
+        <Tab.Screen name="A" component={TestStackA} />
+        <Tab.Screen name="B" component={TestStackB} />
       </Tab.Navigator>
+      {/* <TestStackB /> */}
     </NavCon>
   );
 };
