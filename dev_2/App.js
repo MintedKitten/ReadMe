1fb56a33aae73c97ca2bcd138cddfbd9c563e81a
BuildTools <unconfigured@null.spigotmc.org>
2022-03-14 02:03:44 +0700

    --dev_2 add native-base and add list and display dummy info

--- dev_2/App.js
+++ dev_2/App.js
@@ -1,10 +1,23 @@
 // Invoke-Expression (((ConvertFrom-StringData (Get-Content .\dev_2\android\local.properties -raw)).'sdk.dir')+'\emulator\emulator.exe -avd Pixel_3_API_29')
 import 'react-native-gesture-handler';
-import {Button, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {SafeAreaView, StyleSheet, View, Text, Button} from 'react-native';
 import React, {useState, useEffect, useLayoutEffect} from 'react';
 import RNBootSplash from 'react-native-bootsplash';
 import {NavigationContainer as NavCon} from '@react-navigation/native';
 import {Overlay, Icon} from 'react-native-elements';
+import {
+  NativeBaseProvider,
+  Box,
+  Flex,
+  Spacer,
+  Center,
+  Row,
+  Column,
+  FlatList,
+  Image,
+  ScrollView,
+  Heading,
+} from 'native-base';
 
 const Stack =
   require('@react-navigation/native-stack').createNativeStackNavigator();
@@ -24,7 +37,7 @@ const EditProfilePage = ({navigation}, forceUpdate) => {
             onPress={() => {
               navigation.navigate('Profile');
             }}
-          />{' '}
+          />
         </Text>
       ),
     });
@@ -51,7 +64,7 @@ const ProfilePage = ({navigation}, forceUpdate) => {
             onPress={() => {
               navigation.navigate('EditProfile');
             }}
-          />{' '}
+          />
         </Text>
       ),
     });
@@ -114,22 +127,87 @@ const InformationPage = ({navigation}, forceUpdate) => {
     });
   }, []);
 
+  useEffect(() => {
+    getData();
+  }, []);
+
   const [overlayVisible, setOverlayVisible] = useState(false);
+  const [toOverlay, setToOverlay] = useState({});
+  const [details, setDetails] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const toggleOverlay = () => {
     setOverlayVisible(!overlayVisible);
     forceUpdate();
   };
 
+  const getData = async () => {
+    setLoading(true);
+    setDetails(getBookInformation());
+    setLoading(false);
+    forceUpdate();
+  };
+
+  const _onRefresh = () => {
+    getData();
+  };
+
   return (
     <SafeAreaView style={{flex: 1}}>
       <View>
-        <Text>Book Information Page</Text>
-        <Button
-          title="Open Overlay"
-          onPress={() => {
-            toggleOverlay();
+        <FlatList
+          // data from server
+          data={details}
+          //Extract the key with keyExtractor
+          keyExtractor={(item, index) => {
+            return item.id.toString();
           }}
+          // pull down to refresh
+          onRefresh={() => {
+            _onRefresh();
+          }}
+          // if refrash is true, then don't load just yet
+          refreshing={loading}
+          // render data
+          renderItem={({item, index}) => (
+            <Box flex="1" safeAreaTop>
+              <ScrollView>
+                <Flex direction="row" safeAreaTop>
+                  <Center>
+                    <Image
+                      source={{uri: item.picture.src}}
+                      alt={item.name}
+                      size="md"
+                      borderRadius={10}
+                    />
+                  </Center>
+                  <Column>
+                    <Heading>{item.name}</Heading>
+                    <Text>
+                      {'By ' +
+                        item.author +
+                        '\n' +
+                        item.page +
+                        ' Pages\n' +
+                        item.summary +
+                        '\n' +
+                        item.genre}
+                    </Text>
+                  </Column>
+                  <Column>
+                    <Icon
+                      name="more"
+                      type="material"
+                      onPress={() => {
+                        setToOverlay(getOneBookInformation(item.id));
+                        toggleOverlay();
+                      }}
+                    />
+                  </Column>
+                </Flex>
+              </ScrollView>
+            </Box>
+          )}
         />
         <Overlay
           isVisible={overlayVisible}
@@ -137,12 +215,14 @@ const InformationPage = ({navigation}, forceUpdate) => {
             toggleOverlay();
           }}>
           <View>
-            <Text>This is an Overlay for Book Information</Text>
+            <Text>This is an Overlay for History: </Text>
+            <Text>{JSON.stringify(toOverlay)}</Text>
             <Icon
               name="book-check"
               type="material-community"
               size={20}
               color="black"
+              style={{}}
               onPress={() => {
                 toggleOverlay();
               }}
@@ -152,6 +232,104 @@ const InformationPage = ({navigation}, forceUpdate) => {
       </View>
     </SafeAreaView>
   );
+};
+
+// id, name, author, genre, picture, favorite
+const testBookInformation = [
+  {
+    id: 1,
+    name: 'Book 1',
+    author: 'John Smith',
+    page: 168,
+    summary: 'Outer Space Cadeting',
+    genre: 'Sci-fi, Romance',
+    picture: {
+      src: 'https://firebasestorage.googleapis.com/v0/b/readme-444f4.appspot.com/o/assets%2Fdummy.png?alt=media&token=232056d1-7f3b-4d38-ad49-8634858ec228',
+    },
+    lastedit: new Date().toISOString(),
+  },
+  {
+    id: 2,
+    name: 'Book 2',
+    author: 'William Shakespeare',
+    page: 318,
+    summary: 'History of Japan',
+    genre: 'Asia History',
+    picture: {
+      src: 'https://firebasestorage.googleapis.com/v0/b/readme-444f4.appspot.com/o/assets%2Fdummy_v1.png?alt=media&token=fe80da21-76ad-47ae-b0f5-c782f9fea4fb',
+    },
+    lastedit: new Date().toISOString(),
+  },
+  {
+    id: 3,
+    name: 'Book 3',
+    author: 'Patiparn Techawatcharapaikul',
+    page: 169,
+    summary: 'How to Fish Like a Pro',
+    genre: 'Guide, Life Hack',
+    picture: {
+      src: 'https://firebasestorage.googleapis.com/v0/b/readme-444f4.appspot.com/o/assets%2Fdummy_v2.png?alt=media&token=e527dbb1-0fe6-40fb-b5e0-f3f5e478ee61',
+    },
+    lastedit: new Date().toISOString(),
+  },
+];
+
+// id, bookid, status, pageread, rating
+const testHistory = [
+  {
+    id: 1,
+    bookid: 1,
+    status: 'Complete',
+    pageread: 168,
+    rating: 4.1,
+    lastedit: new Date().toISOString(),
+  },
+  {
+    id: 2,
+    bookid: 2,
+    status: 'Reading',
+    pageread: 158,
+    rating: 4.1,
+    lastedit: new Date().toISOString(),
+  },
+  {
+    id: 3,
+    bookid: 3,
+    status: 'Dropped',
+    pageread: 102,
+    rating: 1,
+    lastedit: new Date().toISOString(),
+  },
+];
+
+const getBookInformation = () => {
+  return testBookInformation;
+};
+
+const getReadingHistory = () => {
+  let tempHistory = [];
+  testHistory.forEach(element => {
+    let info = getBookInformation().find(book => book.id === element.bookid);
+    tempHistory.push({
+      id: element.id,
+      name: info.name,
+      page: info.page,
+      bookid: element.id,
+      status: element.status,
+      pageread: element.pageread,
+      rating: element.rating,
+      picture: info.picture,
+    });
+  });
+  return tempHistory;
+};
+
+const getOneBookInformation = id => {
+  return testBookInformation.find(book => book.id === id);
+};
+
+const getOneReadingHistory = id => {
+  return getReadingHistory().find(hist => hist.id === id);
 };
 
 const HistoryPage = ({navigation}, forceUpdate) => {
@@ -183,22 +361,83 @@ const HistoryPage = ({navigation}, forceUpdate) => {
     });
   }, []);
 
+  useEffect(() => {
+    getData();
+  }, []);
+
   const [overlayVisible, setOverlayVisible] = useState(false);
+  const [toOverlay, setToOverlay] = useState({});
+  const [details, setDetails] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const toggleOverlay = () => {
     setOverlayVisible(!overlayVisible);
     forceUpdate();
   };
 
+  const getData = async () => {
+    setLoading(true);
+    setDetails(getReadingHistory());
+    setLoading(false);
+    forceUpdate();
+  };
+
+  const _onRefresh = () => {
+    getData();
+  };
+
   return (
     <SafeAreaView style={{flex: 1}}>
       <View>
-        <Text>Reading History Page</Text>
-        <Button
-          title="Open Overlay"
-          onPress={() => {
-            toggleOverlay();
+        <FlatList
+          // data from server
+          data={details}
+          //Extract the key with keyExtractor
+          keyExtractor={(item, index) => {
+            return item.bookid.toString();
           }}
+          // pull down to refresh
+          onRefresh={() => {
+            _onRefresh();
+          }}
+          // if refrash is true, then don't load just yet
+          refreshing={loading}
+          // render data
+          renderItem={({item, index}) => (
+            <Box flex="1" safeAreaTop>
+              <ScrollView>
+                <Column my={1}>
+                  <Flex direction="row" safeAreaTop mx={1}>
+                    <Center>
+                      <Image
+                        source={{uri: item.picture.src}}
+                        alt={item.name}
+                        size="md"
+                        borderRadius={10}
+                      />
+                    </Center>
+                    <Column mx={4}>
+                      <Heading>{item.name}</Heading>
+                      <Text>{item.status}</Text>
+                    </Column>
+                    <Column>
+                      <Flex alignItems="flex-end">
+                        <Icon
+                          name="more"
+                          type="material"
+                          color="black"
+                          onPress={() => {
+                            setToOverlay(getOneReadingHistory(item.id));
+                            toggleOverlay();
+                          }}
+                        />
+                      </Flex>
+                    </Column>
+                  </Flex>
+                </Column>
+              </ScrollView>
+            </Box>
+          )}
         />
         <Overlay
           isVisible={overlayVisible}
@@ -206,12 +445,14 @@ const HistoryPage = ({navigation}, forceUpdate) => {
             toggleOverlay();
           }}>
           <View>
-            <Text>This is an Overlay for History</Text>
+            <Text>This is an Overlay for History: </Text>
+            <Text>{JSON.stringify(toOverlay)}</Text>
             <Icon
               name="book-check"
               type="material-community"
               size={20}
               color="black"
+              style={{}}
               onPress={() => {
                 toggleOverlay();
               }}
@@ -467,7 +708,9 @@ const NavigationComponent = () => {
 
   return (
     // for testing Login First then App
-    <>{session.length > 0 ? <AppNavigationTab /> : <LoginNavigationStack />}</>
+    <NativeBaseProvider>
+      {session.length > 0 ? <AppNavigationTab /> : <LoginNavigationStack />}
+    </NativeBaseProvider>
     // <LoginNavigationStack />
     // <AppNavigationTab />
   );
